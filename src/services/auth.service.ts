4cec9de0e461caff7cd716fb54e9823c76429611
Thanh Nguyen <908739+tnsicdr@users.noreply.github.com>
2022-03-21 00:46:03 -0400

    scaffold auth

---
+++ src/services/auth.service.ts
@@ -0,0 +1,9 @@
+
+// TODO: Take inputs and hash passwords/generate set other data for new user
+export const buildNewUser = () => {}
+
+// TODO: Hash and salt password
+export const hashPassword = (password: string) => {}
+
+// TODO: Validate password matches with hashed password
+export const validatePassword = (password: string, hashedPassword: string) => {}
